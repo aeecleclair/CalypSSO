8484c768d1f4e10bf8250391a41351bf4f62147a
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-05-26 20:37:32 +0200

    feat: reset password page

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useSearchParams } from "next/navigation";
+
+const HYPERION = "http://localhost:8000";
+
+const formSchema = z.object({
+  newPassword: z.string().min(6, {
+    message: "Votre mot de passe doit avoir au moins 6 caractères",
+  }),
+});
+
+export function ResetPasswordForm() {
+  let isRegisterLoading = false;
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      newPassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    isRegisterLoading = true;
+
+    fetch(HYPERION + "/users/reset-password", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        reset_token: token,
+        new_password: values.newPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => (isRegisterLoading = true));
+  }
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">MyECL</CardTitle>
+        <CardDescription>Réinitialisez votre mot de passe</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="newPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nouveau mot de passe</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Mot de passe"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
+
+const Reset = () => {
+  return <h1> Test </h1>;
+};
+
+export default ResetPasswordForm;
