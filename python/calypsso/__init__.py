f62a3d210700e051acb7aefda10a551686002de5
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-05-27 08:11:34 +0200

    feat: serve variable.json from Python module

--- python/calypsso/__init__.py
+++ python/calypsso/__init__.py
@@ -6,9 +6,16 @@ import importlib.resources
 MODULE_PATH = importlib.resources.files(__package__)
 
 
-def get_calypsso_app() -> FastAPI:
+def get_calypsso_app(hyperion_url: str) -> FastAPI:
     app = FastAPI()
     print(MODULE_PATH)
+
+    @app.get(
+        "/variables.json",
+        status_code=200,
+    )
+    async def get_variables():
+        return {"hyperion": hyperion_url}
 
     app.mount(
         "/",
