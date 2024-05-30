81230a1f8c9661d19b33d9a6e1a03619c85830c2
Maxime Roucher <maximeroucher@gmail.com>
2024-05-30 18:55:12 +0200

    feat: updating font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import { Background } from "./Background";
 import "./globals.css";
+import Providers from "./providers";
 import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Providers from "./providers";
+import { Signika } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Signika({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
