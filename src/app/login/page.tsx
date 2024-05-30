6e9075142e45ed7b5a72216aff4cf5dc9bf804b7
Maxime Roucher <maximeroucher@gmail.com>
2024-05-30 14:15:49 +0200

    feat: retrieving auth information

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@
 import { BodyAuthorizeValidationAuthAuthorizationFlowAuthorizeValidationPost } from "@/api/hyperionSchemas";
 import { CenteredCard } from "@/components/custom/CenteredCard";
 import { CustomFormField } from "@/components/custom/CustomFormField";
+import { HiddenField } from "@/components/custom/HiddenField";
 import { LoadingButton } from "@/components/custom/LoadingButton";
 import { PasswordInput } from "@/components/custom/PasswordInput";
 import { Button } from "@/components/ui/button";
@@ -30,6 +31,14 @@ const Login = () => {
       .email({
         message: "Veuillez renseigner une adresse email valide",
       }),
+    client_id: z.string(),
+    response_type: z.string(),
+    redirect_uri: z.string(),
+    scope: z.string(),
+    state: z.string(),
+    nonce: z.string(),
+    code_challenge: z.string().optional(),
+    code_challenge_method: z.string().optional(),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -45,28 +54,28 @@ const Login = () => {
       type: "manual",
       message: isError ? "Identifiants incorrects" : "",
     });
-    form.reset({ email: form.getValues("email") });
+    form.reset({ ...form.getValues(), password: "" });
   }, [form, isError]);
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     const body: BodyAuthorizeValidationAuthAuthorizationFlowAuthorizeValidationPost =
       {
-        client_id: "",
-        response_type: "",
+        client_id: values.client_id,
+        response_type: values.response_type,
         email: values.email,
         password: values.password,
-        redirect_uri: "",
-        scope: "",
-        state: "",
-        nonce: "",
+        redirect_uri: values.redirect_uri,
+        scope: values.scope,
+        state: values.state,
+        nonce: values.nonce,
       };
-    // Only for PKCE
-    // if (values.code_challenge) {
-    //   body.code_challenge = values.code_challenge;
-    // }
-    // if (values.code_challenge_method) {
-    //   body.code_challenge_method = values.code_challenge_method;
-    // }
+    // Only for PKCE, if the code_challenge is not present, we don't send it
+    if (values.code_challenge) {
+      body.code_challenge = values.code_challenge;
+    }
+    if (values.code_challenge_method) {
+      body.code_challenge_method = values.code_challenge_method;
+    }
     authenticate(body, () => {
       // redirect to the redirect_uri
     });
@@ -80,6 +89,30 @@ const Login = () => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <div className="grid gap-4">
+            <HiddenField form={form} name="client_id" queryParam="client_id" />
+            <HiddenField
+              form={form}
+              name="response_type"
+              queryParam="response_type"
+            />
+            <HiddenField
+              form={form}
+              name="redirect_uri"
+              queryParam="redirect_uri"
+            />
+            <HiddenField form={form} name="scope" queryParam="scope" />
+            <HiddenField form={form} name="state" queryParam="state" />
+            <HiddenField form={form} name="nonce" queryParam="nonce" />
+            <HiddenField
+              form={form}
+              name="code_challenge"
+              queryParam="code_challenge"
+            />
+            <HiddenField
+              form={form}
+              name="code_challenge_method"
+              queryParam="code_challenge_method"
+            />
             <CustomFormField
               form={form}
               name="email"
