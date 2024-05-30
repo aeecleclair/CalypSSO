f3fa6f6bef27aa4b9ac85e048fab416b61280f4f
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-05-30 18:19:11 +0200

    feat: remove variables.json

--- python/calypsso/__init__.py
+++ python/calypsso/__init__.py
@@ -6,16 +6,9 @@ import importlib.resources
 MODULE_PATH = importlib.resources.files(__package__)
 
 
-def get_calypsso_app(hyperion_url: str) -> FastAPI:
+def get_calypsso_app() -> FastAPI:
     app = FastAPI()
     print(MODULE_PATH)
-
-    @app.get(
-        "/variables.json",
-        status_code=200,
-    )
-    async def get_variables():
-        return {"hyperion": hyperion_url}
 
     app.mount(
         "/",
