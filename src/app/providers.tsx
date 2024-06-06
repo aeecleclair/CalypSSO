4a08175cbbca3c2f77c5569f105c872f4d4ac8da
Maxime Roucher <maximeroucher@gmail.com>
2024-06-06 14:59:57 +0200

    feat: updating provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,56 +1,14 @@
-// In Next.js, this file would be called: app/providers.jsx
 "use client";
 
-import { createClient } from "@hey-api/client-fetch";
-// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// In Next.js, this file would be called: app/providers.jsx
-
-// In Next.js, this file would be called: app/providers.jsx
-
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (typeof window === "undefined") {
-    // Server: always make a new query client
-    return makeQueryClient();
-  } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
+import { toast } from "@/components/ui/use-toast";
+import { createClient, client } from "@hey-api/client-fetch";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
-  const queryClient = getQueryClient();
-
   createClient({
     baseUrl:
       process.env.NEXT_PUBLIC_OVERRIDE_HYPERION_URL ||
       (typeof window !== "undefined" ? window?.location.origin : ""),
   });
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return children;
 }
