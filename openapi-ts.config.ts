033cbaf48fb388e84dc7573cc230e30ed4103676
Maxime Roucher <maximeroucher@gmail.com>
2024-06-08 16:13:04 +0200

    fix: codegen output folder

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
   input: `http://127.0.0.1:8000/openapi.json`,
-  output: "src/api2",
+  output: "src/api",
   client: "@hey-api/client-fetch",
 });
