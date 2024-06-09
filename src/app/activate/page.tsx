d12159c8623d96f470a9e8dceca3cd0de79eb35c
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-06-09 22:54:32 +0200

    Add password validation rules

--- src/app/activate/page.tsx
+++ src/app/activate/page.tsx
@@ -47,7 +47,23 @@ const RegisterPage = () => {
       })
       .min(6, {
         message: "Le mot de passe doit contenir au moins 6 caractères",
-      }),
+      })
+      .refine(
+        (value) => /^.*[A-Z].*$/.test(value),
+        "Le mot de passe doit contenir au moins une majuscule",
+      )
+      .refine(
+        (value) => /^.*[a-z].*$/.test(value),
+        "Le mot de passe doit contenir au moins une minucule",
+      )
+      .refine(
+        (value) => /^.*[0-9].*$/.test(value),
+        "Le mot de passe doit contenir au moins un chiffre",
+      )
+      .refine(
+        (value) => /^.*\[!@#\$%\^&\*\(\),\.\?":{}\|<>]-_.*$/.test(value),
+        'Le mot de passe doit contenir au moins un caractère spécial parmi les suivants [!@#$%^&*(),.?":{}|<>]-_',
+      ),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
