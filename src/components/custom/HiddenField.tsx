9914725a1a59dd0d07c9371ea54bc8b7afd1aa8c
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-06-13 19:20:48 +0200

    feat: redirect to an error page if a required hidden param is missing

--- src/components/custom/HiddenField.tsx
+++ src/components/custom/HiddenField.tsx
@@ -1,19 +1,34 @@
 "use client";
 
 import { FormField } from "../ui/form";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { FieldValues, UseFormReturn } from "react-hook-form";
 
 export interface HiddenFieldProps {
   form: UseFormReturn<any, any, FieldValues | undefined>; // eslint-disable-line @typescript-eslint/no-explicit-any
   queryParam: string;
   name: string;
+  optional?: boolean;
 }
 
-export const HiddenField = ({ form, queryParam, name }: HiddenFieldProps) => {
+export const HiddenField = ({
+  form,
+  queryParam,
+  name,
+  optional = false,
+}: HiddenFieldProps) => {
   const searchParams = useSearchParams();
-  const token = searchParams.get(queryParam);
-  form.setValue(name, token);
+  const value = searchParams.get(queryParam);
+
+  const router = useRouter();
+
+  if (!optional && !value) {
+    router.push(
+      encodeURI(`/error?message=Url invalide : ${queryParam} manquant`),
+    );
+  }
+
+  form.setValue(name, value);
 
   return (
     <FormField
