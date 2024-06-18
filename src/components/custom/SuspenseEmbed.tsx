14ba69b7029c5ca41f2cff755e9d1aa4f08efa8e
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-06-18 17:12:33 +0200

    Remove

--- src/components/custom/SuspenseEmbed.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-
-interface SuspenseEmbedProps {
-  children: React.ReactNode;
-}
-
-export const SuspenseEmbed = ({ children }: SuspenseEmbedProps) => {
-  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
-};
