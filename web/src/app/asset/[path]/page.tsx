df452dfda8338d42ea49797720676a2becf9ab0a
Marc-Andrieu <marc.andrieu@outlook.com>
2025-06-11 23:22:22 +0200

    New page to display nicely Hyperion public text assets

---
+++ web/src/app/asset/[path]/page.tsx
@@ -0,0 +1,34 @@
+import { CenteredCard } from "@/components/custom/CenteredCard";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { notFound } from "next/navigation";
+import Markdown from "react-markdown";
+
+const assets = [
+  "privacy",
+  "terms-and-confitions",
+  "myeclpay-terms-of-service",
+  "support",
+];
+
+export function generateStaticParams() {
+  return assets.map((p) => ({ path: p }));
+}
+
+const assetPage = async (props: { params: { path: string } }) => {
+  const { path } = props.params;
+  if (!assets.includes(path)) notFound(); // https://github.com/vercel/next.js/issues/56253
+  const text = await fetch(
+    process.env.NEXT_PUBLIC_OVERRIDE_HYPERION_URL + "/" + path,
+  ).then((data) => data.text());
+  return (
+    <CenteredCard title="Document" description="Consultez son contenu">
+      <ScrollArea className="rounded-md bg-background h-[62vh]">
+        <article className="mx-3">
+          <Markdown>{text}</Markdown>
+        </article>
+      </ScrollArea>
+    </CenteredCard>
+  );
+};
+
+export default assetPage;
