de6832e3f932c175011d2ed1b6a4724283e217ac
Marc-Andrieu <marc.andrieu@outlook.com>
2025-06-18 15:37:33 +0200

    Test: mouse position efffect (could be refactored)

--- web/src/app/asset/page.tsx
+++ web/src/app/asset/page.tsx
@@ -9,7 +9,9 @@ import {
 import { CenteredCard } from "@/components/custom/CenteredCard";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { RequestResult } from "@hey-api/client-fetch";
+import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
 import { notFound, useSearchParams } from "next/navigation";
+import * as React from "react";
 import { Suspense, useEffect, useState } from "react";
 import Markdown from "react-markdown";
 
@@ -35,15 +37,44 @@ const AssetPageContent = () => {
 };
 
 const AssetPage = () => {
+  const radius = 100; // change this to increase the radius of the hover effect
+  const [visible, setVisible] = React.useState(false);
+
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleMouseMove({ currentTarget, clientX, clientY }: any) {
+    const { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
   return (
     <CenteredCard title="Document" description="Consultez son contenu">
-      <ScrollArea className="rounded-md bg-background h-[62vh]">
-        <article className="mx-3 text-justify">
-          <Suspense>
-            <AssetPageContent />
-          </Suspense>
-        </article>
-      </ScrollArea>
+      <motion.div
+        style={{
+          background: useMotionTemplate`
+              radial-gradient(
+                ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
+                hsl(var(--ring)),
+                transparent 80%
+              )
+            `,
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        className="p-[2px] rounded-lg transition duration-300 group/input"
+      >
+        <ScrollArea className="rounded-md bg-background h-[62vh]">
+          <article className="mx-3 text-justify">
+            <Suspense>
+              <AssetPageContent />
+            </Suspense>
+          </article>
+        </ScrollArea>
+      </motion.div>
     </CenteredCard>
   );
 };
